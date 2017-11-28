f93007eba106edba115d00d1dc669c597c0c29cd
Hack Reactor Student <student@hackreactor.com>
2017-11-28 11:42:07 -0800

    Working on Reduce.

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,19 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+  var result ;  
+    if (n === undefined) {
+      result = array[array.length-1];
+    } else if (n <= 0) {
+      result = [];
+    } else if (n < array.length)  {
+      result = array.slice(array.length-n);
+    } else {
+      result = array;
+    }
+
+    return result;
+    
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +59,18 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+
+    if(Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i],i , collection);
+      }
+    } else {
+      for (var key in collection) {
+        iterator(collection[key],key,collection);
+      }
+    }
+    
+
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,16 +92,52 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    _.each(collection,function(item) {
+      if(test(item) === true){
+        result.push(item);
+      }
+    });
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection,function(item) {
+      return !test(item);
+    });
+    
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    var result = [];
+    if(!iterator && isSorted && array.length > 0) {
+      result.push(array[0]);
+      for(var i = 1; i < array.length; i++) {
+        if(array[i] !== result[result.length-1]) {
+          result.push(array[i]);
+        }
+      }
+    } else if (!iterator && !isSorted && array.length > 0) {
+      result.push(array[0]);
+      for(var i = 1; i < array.length; i++) {
+        if(_.indexOf(result,array[i]) === -1) {
+          result.push(array[i]);
+        }
+      }
+    } else if (iterator && isSorted) {
+      result.push(array[0]);
+      for(var i = 1; i < array.length; i++) {
+        if(iterator(array[i]) !== iterator(result[result.length-1])) {
+          result.push(array[i]);
+        }
+      }
+    }
+
+    return result;
   };
 
 
@@ -84,6 +146,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var result = [];
+    _.each(collection, function(value) {
+      result.push(iterator(value));
+    });
+    return result;
   };
 
   /*
@@ -125,6 +192,22 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    var accumulator;
+    if (Array.isArray(collection)) {
+      accumulator = accumulator || collection[0];
+      _.each(collection, function(value) {
+        accumulator = iterator(accumulator, value);
+      });
+
+    } else {
+      accumulator = accumulator || collection[Object.keys(collection)[0]];
+      _.each(collection, function(value) {
+        accumulator = iterator(accumulator, value);
+      });
+    }
+
+    return accumulator;
+
   };
 
   // Determine if the array or object contains a given value (using `===`).
